847c722f368fdc2d4caa9e2e39bc1330da856806
ProtossLuigi <protossluigi@gmail.com>
2023-06-28 15:28:49 +0200

    bugfix

--- stachometr.py
+++ stachometr.py
@@ -2,12 +2,12 @@ import sys
 from datetime import datetime
 
 def main(*args, **kwargs):
-    if len(args) == 0 or args[0].lower() not in ['false', 'true']:
+    if len(args) < 2 or args[1].lower() not in ['false', 'true']:
         print('usage: python stachometr.py <true | false>')
         return
     
     with open('rzuty.csv', 'a') as f:
-        f.write(str(datetime.now()) + ',' + str(args[0].lower() == 'true') + '\n')
+        f.write(str(datetime.now()) + ',' + str(args[1].lower() == 'true') + '\n')
 
 if __name__ == '__main__':
     main(*sys.argv)
